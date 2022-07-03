69b51bf3633cd5cdd24cf970b4427fa85e75f5ca
Sasil21 <108604427+Sasil21@users.noreply.github.com>
2022-07-03 22:25:46 +0530

    second commit

--- docusaurus-exapmle/my-website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('C:\\Users\\Sasi Kumar\\Documents\\GitHub\\myportfolio\\docusaurus-exapmle\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\Sasi Kumar\\Documents\\GitHub\\myportfolio\\docusaurus-exapmle\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\Sasi Kumar\\Documents\\GitHub\\myportfolio\\docusaurus-exapmle\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('C:\\Users\\Sasi Kumar\\Documents\\GitHub\\myportfolio\\docusaurus-exapmle\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\Sasi Kumar\\Documents\\GitHub\\myportfolio\\docusaurus-exapmle\\my-website\\src\\css\\custom.css'),
-];
